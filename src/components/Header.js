79d8c4e0916ad7376f4451d587ce3bef0052ee91
Max Berkenstam <max.berkenstam@gmail.com>
2018-03-20 13:47:30 +0100

    add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import MenuButton from './MenuButton';
 import './Header.css';
+import logo from '../assets/logo.svg';
 
 
 const Header = () => (
   <div className="header-container">
     <MenuButton name="MENY"></MenuButton>
-    <div>SJ-IKON</div>
+    <img src={logo} alt="SJ" />
     <MenuButton name="LOGGA IN">LOGGA IN</MenuButton>
   </div>
     );
