1937789aa51abf60c33bebf7152a060d16b05fc6
Camilla Tranberg <camillatranberg@Camillas-MacBook-Pro.local>
2018-03-20 16:11:08 +0100

    add menu text

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,20 @@ import logo from '../assets/logo.svg';
 import user from '../assets/user.svg';
 import menuIcon from '../assets/menu-button.svg';
 
-
 const Header = () => (
   <div className="header-container">
-    <MenuButton><img src={menuIcon} alt="Meny" /></MenuButton>
-    <img src={logo} alt="SJ" />
-    <MenuButton><img src={user} alt="Min profil" /></MenuButton>
+    <div className="menu-container">
+      <MenuButton><img src={menuIcon} alt="Meny" />
+        <h3 className="menu-text">Meny</h3>
+      </MenuButton>
+    </div>
+    <div className="logo-container">
+      <img src={logo} alt="SJ" />
+    </div>
+    <div className="profile-container">
+      <MenuButton><img src={user} alt="Min profil" /></MenuButton>
+    </div>
   </div>
-    );
+);
 
 export default Header;
