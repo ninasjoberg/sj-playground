d5eeec5e51baf9f80e0ac932e6622b158617cf61
Camilla Tranberg <camillatranberg@Camillas-MacBook-Pro.local>
2018-03-20 14:18:41 +0100

    Breadcrumbs and heading.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
+import Breadcrumbs from './components/Breadcrumbs/Breadcrumbs';
+import Heading from './components/Heading/Heading';
+import SeatSelection from './components/SeatSelection/SeatSelection';
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      activeBreadcrumb: 3
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <Header></Header>
+        <Header />
+        <Breadcrumbs activeBreadcrumb={this.state.activeBreadcrumb} />
+        <Heading />
+        <SeatSelection />
       </div>
     );
   }
